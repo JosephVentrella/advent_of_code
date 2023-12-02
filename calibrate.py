ad2c48558d7af3394460b1f0ecf6171054bd0696
JosephVentrella <ventrellajoe1@gmail.com>
2023-12-01 19:09:44 -0800

    Implement extract_numbers Implement function to extract numbers from the coordinates string

--- calibrate.py
+++ calibrate.py
@@ -1,3 +1,17 @@
+import re
+
+def extract_numbers(line):
+    return re.sub(r"\D", "", line)
+    
 with open('calibration_doc.txt', 'r') as file:
     lines = file.readlines()
-print(lines)
+
+final_answer = 0
+for i in lines:
+    val = (extract_numbers(i))
+    # print(val)
+    final_coordinate = val[0] + val[-1]
+    final_coordinate = int(final_coordinate)
+    print(final_coordinate)
+    final_answer += final_coordinate
+print(f"Final answer is: {final_answer}")
