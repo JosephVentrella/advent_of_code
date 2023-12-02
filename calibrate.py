4cf950265fb60d41cfeb86759fc54ed38f457106
JosephVentrella <ventrellajoe1@gmail.com>
2023-12-01 19:14:29 -0800

    Refactor naming Make variable intent more obvious

--- calibrate.py
+++ calibrate.py
@@ -1,6 +1,6 @@
 import re
 
-def extract_numbers(line):
+def extract_coordinates(line):
     return re.sub(r"\D", "", line)
     
 with open('calibration_doc.txt', 'r') as file:
@@ -8,10 +8,7 @@ with open('calibration_doc.txt', 'r') as file:
 
 final_answer = 0
 for i in lines:
-    val = (extract_numbers(i))
-    # print(val)
-    final_coordinate = val[0] + val[-1]
-    final_coordinate = int(final_coordinate)
-    print(final_coordinate)
-    final_answer += final_coordinate
+    hidden_coordinates = (extract_coordinates(i))
+    final_coordinate = hidden_coordinates[0] + hidden_coordinates[-1]
+    final_answer += int(final_coordinate)
 print(f"Final answer is: {final_answer}")
