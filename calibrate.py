ed44866e0a7157a89b2d617b4c1a60a2b551c4dc
JosephVentrella <ventrellajoe1@gmail.com>
2023-12-01 19:59:20 -0800

    [calibrate.py] Create word to digit map Create a map that helps convert words to digits

--- calibrate.py
+++ calibrate.py
@@ -2,6 +2,14 @@ import re
 
 def extract_coordinates(line):
     return re.sub(r"\D", "", line)
+
+def word_to_digit(word):
+    digit_map = {
+        "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+        "six": 6, "seven": 7, "eight": 8, "nine": 9
+    }
+    return mapping.get(word.lower(), "")
+
     
 with open('calibration_doc.txt', 'r') as file:
     lines = file.readlines()
