5ba5e6e0f4571f6b1c68b6e323b505ce0d1dd28f
JosephVentrella <ventrellajoe1@gmail.com>
2023-12-01 20:00:16 -0800

    Fix typo Fix variable name that didnt get updated

--- calibrate.py
+++ calibrate.py
@@ -8,7 +8,7 @@ def word_to_digit(word):
         "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
         "six": 6, "seven": 7, "eight": 8, "nine": 9
     }
-    return mapping.get(word.lower(), "")
+    return digit_map.get(word.lower(), "")
 
     
 with open('calibration_doc.txt', 'r') as file:
